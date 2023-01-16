6cc7dde7aeb9b071dd0a6e07c3cee679e3798b25
dell-zs <someone000@126.com>
2023-01-16 21:39:13 +0800

    init

--- src/test/java/com/codeja/activiti/test/TestDeoployment.java
+++ src/test/java/com/codeja/activiti/test/TestDeoployment.java
@@ -1,11 +1,14 @@
 package com.codeja.activiti.test;
 
-import org.activiti.engine.ProcessEngine;
-import org.activiti.engine.ProcessEngines;
-import org.activiti.engine.RepositoryService;
+import org.activiti.engine.*;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.repository.DeploymentBuilder;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
+import org.activiti.engine.task.TaskQuery;
 import org.junit.Test;
+
+import java.util.List;
 
 public class TestDeoployment {
 
@@ -40,6 +43,67 @@ public class TestDeoployment {
         System.out.println("leave.getId() = " + leave.getId());
         System.out.println("leave.getName() = " + leave.getName());
         System.out.println("leave.getCategory() = " + leave.getCategory());
-
     }
+
+
+    /**
+     * 启动流程实例
+     * act_hi_actinst
+     * act_hi_identitylink
+     * act_hi_procinst  流程实例的历史信息
+     * act_hi_taskinst  任务的历史信息
+     * act_ru_execution 流程执行信息
+     * act_ru_identitylink  流程参与者信息
+     * act_ru_task 任务信息
+     */
+    @Test
+    public void testStartProcess() {
+
+        //1. 创建ProcessEngine
+
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+
+        //2. 获取RuntimeService
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+
+        //3. 根据流程的id启动流程
+        ProcessInstance instance = runtimeService.startProcessInstanceByKey("myEvection");
+
+        //4.输出内容
+        System.out.println("instance.getProcessDefinitionId() = " + instance.getProcessDefinitionId());
+
+        System.out.println("instance.getId() = " + instance.getId());
+
+        System.out.println("instance.getActivityId() = " + instance.getActivityId());
+    }
+
+
+    /**
+     * 查询个人待执行的任务
+     */
+    @Test
+    public void testFindPersonTaskList() {
+
+        //1.获取流程引擎
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+
+        //2.获取taskService
+        TaskService taskService = processEngine.getTaskService();
+
+
+        //3.根据key和任务的负责人，查询任务
+        List<Task> aaa = taskService.createTaskQuery()
+                .processDefinitionKey("aaa")
+                .taskAssignee("")
+                .list();
+
+        //4. 输出
+        for (Task task : aaa) {
+            System.out.println("task.getProcessInstanceId() = " + task.getProcessInstanceId());
+            System.out.println("task.getId() = " + task.getId());
+            System.out.println("task.getAssignee() = " + task.getAssignee());
+            System.out.println("task.getName() = " + task.getName());
+        }
+    }
+
 }
---
+++ src/test/java/com/codeja/activiti/test/TestDeployment2.java
@@ -0,0 +1,16 @@
+package com.codeja.activiti.test;
+
+import org.junit.Test;
+
+/**
+ * https://www.bilibili.com/video/BV1Wf4y1879C?p=5&vd_source=dd9e0d42756c3d61df99de756c651fb9
+ *
+ */
+public class TestDeployment2 {
+
+
+    @Test
+    public void testDeployment(){
+
+    }
+}
